4fa1134d8f8201f13788904418b1b018af755c7d
aarslanzakir <aliarslanzakir@gmail.com>
2025-08-10 01:07:56 -0700

    Added

--- src/pages/CreateFalcons.tsx
+++ src/pages/CreateFalcons.tsx
@@ -7,7 +7,7 @@ import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { Plus, ImagePlus } from "lucide-react";
 import { useToast } from "@/hooks/use-toast";
-
+//Falcons
 interface Falcon {
   id: string;
   name_ar: string;
